f2eb52d1a0f0a6549e0ed67d417efda5694d70db
Gokul-J <jgokul2000@gmail.com>
2020-08-23 21:55:06 +0530

    Updates

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -14,60 +14,60 @@ export default function Skills({ id }) {
         {/* <p>Iam a MERN Stack Web </p> */}
         <div className="row">
           <div className="col-4 col-sm-4 col-md-3 mb-3 mb-md-0">
-            <div id="hexagon"><i className="skills-icon hover-effect fab fa-html5"></i></div>
+            <div id="hexagon"><i className="skills-icon fab fa-html5"></i></div>
             <p className="title text-center">HTML</p>
           </div>
           <div className="col-4 col-sm-4 col-md-3 mb-3 mb-md-0">
-            <div id="hexagon"><i className="skills-icon hover-effect fab fa-css3-alt"></i></div>
+            <div id="hexagon"><i className="skills-icon fab fa-css3-alt"></i></div>
             <p className="title text-center">CSS</p>
           </div>
           <div className="col-4 col-sm-4 col-md-3 mb-3 mb-md-0">
-            <div id="hexagon"><i className="skills-icon hover-effect fab fa-js-square"></i></div>
+            <div id="hexagon"><i className="skills-icon fab fa-js-square"></i></div>
             <p className="title text-center">JavaScript</p>
 
           </div>
           <div className="col-4 col-sm-4 col-md-3 mb-3 mb-md-0">
-            <div id="hexagon"><i className="skills-icon hover-effect fab fa-bootstrap"></i></div>
+            <div id="hexagon"><i className="skills-icon fab fa-bootstrap"></i></div>
             <p className="title text-center">Bootstrap</p>
 
           </div>
           <div className="col-4 col-sm-4 col-md-3 mb-3 mb-md-0">
-            <div id="hexagon"><i className="skills-icon hover-effect fab fa-react"></i></div>
+            <div id="hexagon"><i className="skills-icon fab fa-react"></i></div>
             <p className="title text-center">React</p>
 
           </div>
           <div className="col-4 col-sm-4 col-md-3 mb-3 mb-md-0">
-            <div id="hexagon"><img src={redux} alt="" className="redux hover-effect" /></div>
+            <div id="hexagon"><img src={redux} alt="" className="redux" /></div>
             <p className="title text-center">Redux</p>
 
           </div>
           <div className="col-4 col-sm-4 col-md-3 mb-3 mb-md-0">
-            <div id="hexagon"><i className="skills-icon hover-effect fab fa-node"></i></div>
+            <div id="hexagon"><i className="skills-icon fab fa-node"></i></div>
             <p className="title text-center">Nodejs<br/>Expressjs</p>
 
           </div>
           <div className="col-4 col-sm-4 col-md-3 mb-3 mb-md-0">
-            <div id="hexagon"><img src={mongodb} alt="" className="mongodb hover-effect" /></div>
+            <div id="hexagon"><img src={mongodb} alt="" className="mongodb" /></div>
             <p className="title text-center">MongoDB</p>
 
           </div>
           <div className="col-4 col-sm-4 col-md-3 mb-3 mb-md-0">
-            <div id="hexagon"><i className="skills-icon hover-effect fab fa-github"></i></div>
+            <div id="hexagon"><i class="skills-icon fab fa-git-alt"></i></div>
             <p className="title text-center">GIT</p>
 
           </div>
           <div className="col-4 col-sm-4 col-md-3 mb-3 mb-md-0">
-            <div id="hexagon"><i className="skills-icon hover-effect fab fa-java"></i></div>
+            <div id="hexagon"><i className="skills-icon fab fa-java"></i></div>
             <p className="title text-center">Java</p>
 
           </div>
           <div className="col-4 col-sm-4 col-md-3 mb-3 mb-md-0">
-            <div id="hexagon"><i className="skills-icon hover-effect fab fa-python"></i></div>
+            <div id="hexagon"><i className="skills-icon fab fa-python"></i></div>
             <p className="title text-center">Python</p>
 
           </div>
           <div className="col-4 col-sm-4 col-md-3 mb-3 mb-md-0">
-            <div id="hexagon"><img src={restapi} alt="" className="restapi hover-effect" /></div>
+            <div id="hexagon"><img src={restapi} alt="" className="restapi" /></div>
             <p className="title text-center">REST API</p>
           </div>
         </div>
